84bf6927b64ae037b7e215d998b8a2914beaadf7
Negredo <jonathannegredo@gmail.com>
2024-09-15 11:37:31 -0300

    fundamento 003

--- index.js
+++ index.js
@@ -17,19 +17,37 @@
 //  let metas = ["negredo", "alô"]
 
 //  console.log(metas[1] + ", " + metas[0])
+//----------------------------------------------
+// let meta = {
+//     value: 'Ler um livro por mês',
+//     checked: false,
+//     isChecked: (info) => {
+//         console.log(info)
+//     }
+// }
 
-let meta = {
-    value: 'Ler um livro por mês',
-    checked: false,
-    isChecked: (info) => {
-        console.log(info)
-    }
-}
-
-meta.value = 'não é mais ler um livro'
-meta.isChecked(meta.value)
+// meta.value = 'não é mais ler um livro'
+// meta.isChecked(meta.value)
 
 //function      // arrow function
 //const criarMeta = () => {}
 //ou
 //function criarMeta() {}
+
+//-------------------------------------------
+let meta = {
+    value: 'Ler um livro por mês',
+    checked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todo dia",
+        checked: false
+    }
+]
+
+console.log(meta.value)
+console.log(metas[1].value)
+console.log(metas[0].value)
